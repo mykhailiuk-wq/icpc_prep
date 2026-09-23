c009561274ee996ed2006ab7a943d2db2b4d53c0
mykhailiuk-wq <vitalii270406@gmail.com>
2026-09-23 22:06:04 +0300

    Refactor functions into Solution class and add new methods
    Refactored isAnagram and getFrequency functions into a Solution class. Added isPalindrome and hasDuplicate functions for additional functionality.

--- prep.cpp
+++ prep.cpp
@@ -1,41 +1,63 @@
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
-
-bool isAnagram(string s, string t) {
-    sort(s.begin(), s.end());
-    sort(t.begin(), t.end());
-    if (s == t){
-        return true;
-    } else {
-        return false;
-    }
-}
-
-unordered_map<char, int> getFrequency(string s) {
-    unordered_map<char, int> freq;
-    unordered_set<char> unique;
-    for (char sym : s) {
-        if (unique.find(sym) == unique.end()){
-            freq[sym]++; 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if (s == t){
+            return true;
         } else {
-            unique.insert(sym);
-            freq[sym] = 0;
+            return false;
         }
     }
-    return freq;
-}
 
-bool hasDuplicate(vector<int>& nums) {  
-  unordered_set<int> unique;
-  for (int num : nums) {
-    if (unique.find(num) == unique.end()) {
-      unique.insert(num);
-    } else {
-      return true;
+    unordered_map<char, int> getFrequency(string s) {
+        unordered_map<char, int> freq;
+        unordered_set<char> unique;
+        for (char sym : s) {
+            if (unique.find(sym) == unique.end()){
+                freq[sym]++; 
+            } else {
+                unique.insert(sym);
+                freq[sym] = 0;
+            }
+        }
+        return freq;
     }
-   }
-   return false;
+
+    bool hasDuplicate(vector<int>& nums) {  
+      unordered_set<int> unique;
+      for (int num : nums) {
+        if (unique.find(num) == unique.end()) {
+          unique.insert(num);
+        } else {
+          return true;
+        }
+       }
+       return false;
+    }
+
+    bool isPalindrome(string s) {
+        string cutString = "";
+        for (char sym : s) {
+            sym = tolower(sym);
+            if (isalnum(sym)) {
+                cutString.push_back(sym);
+            }
+        }
+        char front;
+        char back;
+        for (int i = 0; i < cutString.length() / 2; i++) { 
+            front = cutString[i];
+            back = cutString[cutString.length() - i - 1];
+            if (front != back) {
+                return false;
+            }
+        } 
+        return true;
+    }
 }
 
 int main(){}
